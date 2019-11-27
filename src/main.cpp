b124ddc39d1e27f7d617628feb626e7b1f644349
pdthang <pdthang59@gmail.com>
2019-11-27 22:34:52 +0100

    Add temperature thermistor sensor

--- src/main.cpp
+++ src/main.cpp
@@ -24,40 +24,50 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+#include "Thermistor.h"
 
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristic = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-uint32_t value = 0;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-
+#define ms_TO_S_FACTOR 1000 /* Conversion factor for mili seconds to seconds */
+#define TIME_INTERVAL  3 /* Time interval ESP32 will send data and repeat (in seconds) */
+#define SENSOR_PIN 36
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
-      Serial.println("deviceConnected");
+      Serial.println("[INFO] deviceConnected");
     };
 
     void onDisconnect(BLEServer* pServer) {
       deviceConnected = false;
-      Serial.println("deviceDisconnected");
+      Serial.println("[INFO] deviceDisconnected");
     }
 };
 
-
+/*
+  Thermistor sensor initializes
+*/
+THERMISTOR thermistor(SENSOR_PIN,        // Analog pin
+                      10000,          // Nominal resistance at 25 ºC
+                      3950,           // thermistor's beta coefficient
+                      10000);         // Value of the series resistor
+// Global temperature reading
+float temp;
 
 void setup() {
   Serial.begin(115200);
 
   // Create the BLE Device
-  String serverName = "ESP32-";
-  serverName += String(random(0xffff), HEX);
+  String serverName = "ESP32-02";
+  //serverName += String(random(0xffff), HEX);
   BLEDevice::init(serverName.c_str());
 
   // Create the BLE Server
@@ -89,27 +99,30 @@ void setup() {
   pAdvertising->setScanResponse(false);
   pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
-  Serial.println("Waiting a client connection to notify...");
+  Serial.println("[INFO] waiting a client connection to notify...");
 }
 
 void loop() {
-    // notify changed value
-    if (deviceConnected) {
-        pCharacteristic->setValue((uint8_t*)&value, 4);
+    
+      // notify changed value
+      if (deviceConnected) {
+        temp = thermistor.read();
+        pCharacteristic->setValue(temp);
         pCharacteristic->notify();
-        value++;
-        delay(2000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
-    }
-    // disconnecting
-    if (!deviceConnected && oldDeviceConnected) {
+        Serial.println("[INFO] sent data");
+        delay(TIME_INTERVAL * ms_TO_S_FACTOR);
+      }
+      // disconnecting
+      if (!deviceConnected && oldDeviceConnected) {
         delay(500); // give the bluetooth stack the chance to get things ready
         pServer->startAdvertising(); // restart advertising
-        Serial.println("start advertising");
+        Serial.println("[INFO] start advertising");
         oldDeviceConnected = deviceConnected;
-    }
-    // connecting
-    if (deviceConnected && !oldDeviceConnected) {
-        // do stuff here on connecting
+      }
+      // connecting
+      if (deviceConnected && !oldDeviceConnected) {
+          // do stuff here on connecting
         oldDeviceConnected = deviceConnected;
-    }
+      }
+    
 }
