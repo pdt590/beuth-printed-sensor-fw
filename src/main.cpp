4750a08a0537ef2832ebcd3a9bcb07a437b2a693
pdthang <pdthang59@gmail.com>
2020-03-03 16:05:20 +0100

    Init LIS3DH and BME680 code

--- src/main.cpp
+++ src/main.cpp
@@ -1,134 +1,97 @@
 /*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-    updated by chegewara
-
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 4fafc201-1fb5-459e-8fcc-c5c9c331914b
-   And has a characteristic of: beb5483e-36e1-4688-b7f5-ea07361b26a8
-
-   The design of creating the BLE server is:
-   1. Create a BLE Server
-   2. Create a BLE Service
-   3. Create a BLE Characteristic on the Service
-   4. Create a BLE Descriptor on the characteristic
-   5. Start the service.
-   6. Start advertising.
-
-   A connect hander associated with the server starts a background task that performs notification
-   every couple of seconds.
+** References
+** - https://randomnerdtutorials.com/esp32-i2c-communication-arduino-ide/
+** - https://learn.adafruit.com/adafruit-lis3dh-triple-axis-accelerometer-breakout?view=all
+** - https://learn.adafruit.com/adafruit-bme680-humidity-temperature-barometic-pressure-voc-gas?view=all
+**
 */
+
 #include <Arduino.h>
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
-#include "Thermistor.h"
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_BME680.h>
 
-BLEServer *pServer = NULL;
-BLECharacteristic *pCharacteristic = NULL;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
+// Initialize LIS3DH
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
+// Initialize BME680
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BME680 bme;
 
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-#define ms_TO_S_FACTOR 1000 /* Conversion factor for mili seconds to seconds */
-#define TIME_INTERVAL 3     /* Time interval ESP32 will send data and repeat (in seconds) */
-#define SENSOR_PIN 36
-
-class MyServerCallbacks : public BLEServerCallbacks
+void setup(void)
 {
-  void onConnect(BLEServer *pServer)
-  {
-    deviceConnected = true;
-    Serial.println("[INFO] deviceConnected");
-  };
+  Serial.begin(9600);
+  while (!Serial)
+    delay(10); // will pause until serial console opens
 
-  void onDisconnect(BLEServer *pServer)
+  Serial.println("LIS3DH & BME680 starting...!");
+
+  // LIS3DH supports 0x18 (default) or 0x19 address
+  // BME680 supports 0x77 (default) or 0x76 address
+  if (!lis.begin(0x18) || !bme.begin(0x77))
   {
-    deviceConnected = false;
-    Serial.println("[INFO] deviceDisconnected");
+    Serial.println("Couldn't start...");
+    while (1)
+      yield();
   }
-};
+  Serial.println("LIS3DH & BME680 found!");
 
-/*
-  Thermistor sensor initializes
-*/
-THERMISTOR thermistor(SENSOR_PIN, // Analog pin
-                      10000,      // Nominal resistance at 25 ºC
-                      3950,       // thermistor's beta coefficient
-                      10000);     // Value of the series resistor
-// Global temperature reading
-float temp;
+  // Set up LIS3DH (accelerometer range)
+  lis.setRange(LIS3DH_RANGE_4_G); // 2, 4, 8 or 16 G!
 
-void setup()
-{
-  Serial.begin(115200);
-
-  // Create the BLE Device
-  String serverName = "ESP32-02";
-  //serverName += String(random(0xffff), HEX);
-  BLEDevice::init(serverName.c_str());
-
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-
-  // Create a BLE Characteristic
-  pCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_READ |
-          BLECharacteristic::PROPERTY_WRITE |
-          BLECharacteristic::PROPERTY_NOTIFY |
-          BLECharacteristic::PROPERTY_INDICATE);
-
-  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
-  // Create a BLE Descriptor
-  pCharacteristic->addDescriptor(new BLE2902());
-
-  // Start the service
-  pService->start();
-
-  // Start advertising
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(false);
-  pAdvertising->setMinPreferred(0x0); // set value to 0x00 to not advertise this parameter
-  BLEDevice::startAdvertising();
-  Serial.println("[INFO] waiting a client connection to notify...");
+  // Set up BME680 (oversampling and filter initialization)
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
 }
 
 void loop()
 {
+  // LIS3DH displays values (acceleration is measured in m/s^2)
+  sensors_event_t event;
+  lis.getEvent(&event);
+  Serial.print("\t\tX: ");
+  Serial.print(event.acceleration.x);
+  Serial.print(" \tY: ");
+  Serial.print(event.acceleration.y);
+  Serial.print(" \tZ: ");
+  Serial.print(event.acceleration.z);
+  Serial.println(" m/s^2 ");
 
-  // notify changed value
-  if (deviceConnected)
+  Serial.println();
+
+  // BME680 displays values
+  if (!bme.performReading())
   {
-    temp = thermistor.read();
-    pCharacteristic->setValue(temp);
-    pCharacteristic->notify();
-    Serial.println("[INFO] sent data");
-    delay(TIME_INTERVAL * ms_TO_S_FACTOR);
+    Serial.println("Failed to perform reading :(");
+    return;
   }
-  // disconnecting
-  if (!deviceConnected && oldDeviceConnected)
-  {
-    delay(500);                  // give the bluetooth stack the chance to get things ready
-    pServer->startAdvertising(); // restart advertising
-    Serial.println("[INFO] start advertising");
-    oldDeviceConnected = deviceConnected;
-  }
-  // connecting
-  if (deviceConnected && !oldDeviceConnected)
-  {
-    // do stuff here on connecting
-    oldDeviceConnected = deviceConnected;
-  }
+  Serial.print("Temperature = ");
+  Serial.print(bme.temperature);
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.humidity);
+  Serial.println(" %");
+
+  Serial.print("Gas = ");
+  Serial.print(bme.gas_resistance / 1000.0);
+  Serial.println(" KOhms");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.println();
+
+  // delay
+  delay(2000);
 }
